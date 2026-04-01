93c00fc248fb76fa1edfc2792bb67b26db968795
samloi <sloi@ucsb.edu>
2026-04-01 14:37:48 -0700

    Delete Lab02 directory

--- Lab02/Tea.py
+++
@@ -1,8 +0,0 @@
-from Drink import Drink 
-
-class Tea(Drink):
-    def __init__ (self, size, price, style):
-        super().__init__(size, price)
-        self.style = style 
-    def info(self):
-        return f"{self.style} Tea, {super().info()}"
